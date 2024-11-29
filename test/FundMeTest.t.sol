f231135218ee239ed9b9f99c937f1eebbcc451ec
Connor Payne <connor@stakehauslabs.com>
2024-11-29 12:05:20 +0100

    mock price feed

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -2,10 +2,28 @@
 
 pragma solidity ^0.8.18;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console } from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
-    function setUp() external{}
+    FundMe fundMe;
 
-    function testDemo() public {}
+    function setUp() external{
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+    }
+
+    function testMinimumDollarIsFive() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public view {
+        assertEq(fundMe.i_owner(), msg.sender);
+    }
+
+    function testPriceFeedVersionIsAccurate() public view {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
+    }
 }
